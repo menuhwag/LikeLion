dfe8b4a438eea5258ad34399b9efdaadb0994e9d
menuhwang <hmin0206@naver.com>
2022-10-05 10:47:17 +0900

    생성자 사용

--- src/practice/java_221005/Calculator.java
+++ src/practice/java_221005/Calculator.java
@@ -1,16 +1,24 @@
 package practice.java_221005;
 
 public class Calculator {
-    public int plus(int a, int b) {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int plus() {
         return a + b;
     }
-    public int minus(int a, int b) {
+    public int minus() {
         return a - b;
     }
-    public int multiple(int a, int b) {
+    public int multiple() {
         return a * b;
     }
-    public double divide(int a, int b) {
+    public double divide() {
         return a / (double)b;
     }
 }
