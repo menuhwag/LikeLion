4c33d52f065ff31028c4242ef8ec9d58130004ad
MinwooHwang <77231274+menuhwang@users.noreply.github.com>
2022-10-05 17:38:01 +0900

    Update Main.java
    - 설명 주석 수정

--- src/homework/homework_221005/interface_practice/Main.java
+++ src/homework/homework_221005/interface_practice/Main.java
@@ -3,7 +3,7 @@ package homework.homework_221005.interface_practice;
 public class Main {
     public static void main(String[] args) { // 면적 계산기 구동 클래스
         Shape rectangle = new Rectangle(2, 5); // 구현체 객체 생성 파라미터 (가로, 세로)
-//        Shape triangle = new Triangle(2, 5); // 파라미터 (밑변, 높이)
+//        Shape triangle = new Triangle(2, 5); // 구현체 객체 파라미터 (밑변, 높이)
 //        Shape circle = new Circle(5); // 구현체 객체 생성 파라미터 (반지름)
 
         ShapeAreaCalculator recAreaCalculator = new ShapeAreaCalculator(rectangle); // 구현체 주입하여 계산기 객체 생성
