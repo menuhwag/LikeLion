45b3c189372c05a0abe532be5b7dadbd8288a2f5
menuhwang <hmin0206@naver.com>
2022-10-05 17:25:49 +0900

    Interface 사용 예제 구현
    - Shape 인터페이스
    - ShapeAreaCalculator 면적 계산기 클래스
    - Main ShapeAreaCalculator에 Shape 객체를 주입하여 테스트하는 클래스
    - Rectangle, Triangle, Circle 객체를 구현하고 주입하여 동작해 볼 것.

---
+++ src/homework/homework_221005/interface_practice/Main.java
@@ -0,0 +1,18 @@
+package homework.homework_221005.interface_practice;
+
+public class Main {
+    public static void main(String[] args) { // 면적 계산기 구동 클래스
+        Shape rectangle = new Rectangle(2, 5); // 구현체 객체 생성 파라미터 (가로, 세로)
+//        Shape triangle = new Triangle(2, 5); // 파라미터 (밑변, 높이)
+//        Shape circle = new Circle(5); // 구현체 객체 생성 파라미터 (반지름)
+
+        ShapeAreaCalculator recAreaCalculator = new ShapeAreaCalculator(rectangle); // 구현체 주입하여 계산기 객체 생성
+        recAreaCalculator.printArea();
+
+        /*
+        위와 같이 삼각형, 원의 면적을 구해보자.
+        삼각형, 원 객체는 Shape 인터페이스를 구현할 것.
+        ShapeAreaCalculator 객체 생성 시 해당 구현체 (삼각형, 원과 같은)를 주입할 것.
+        */
+    }
+}
---
+++ src/homework/homework_221005/interface_practice/Shape.java
@@ -0,0 +1,5 @@
+package homework.homework_221005.interface_practice;
+
+public interface Shape { // 인터페이스 작성
+    double calculateArea();
+}
