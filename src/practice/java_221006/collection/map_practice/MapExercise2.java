f6cc1b0b85bd7dcb2618a9cd4d5887f8dde055bb
menuhwang <hmin0206@naver.com>
2022-10-06 13:17:59 +0900

    알파벳 구분 로직 분리

--- src/practice/java_221006/collection/map_practice/MapExercise2.java
+++ src/practice/java_221006/collection/map_practice/MapExercise2.java
@@ -7,12 +7,16 @@ public class MapExercise2 {
         String repo = "https://github.com/menuhwang/LikeLion".toLowerCase(); // 대문자를 소문자로 변환
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < repo.length(); i++) { // repo 길이만큼 반복
-            if (repo.charAt(i) >= 'a' && repo.charAt(i) <= 'z') { // 문자인지 아닌지 구분
+            if (isAlphabet(repo.charAt(i))) { // 문자인지 아닌지 구분
                 map.put(repo.charAt(i), map.getOrDefault(repo.charAt(i), 0) + 1);
                 // getOrDefault로 값을 가져온다, 없으면 0을 리턴해준다.
                 // 그리고 +1 한 값으로 설정해준다.
             }
         }
         System.out.println(map);
+    }
+
+    public static boolean isAlphabet(char ch) { // 문자인지 아닌지 구분
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
     }
 }
