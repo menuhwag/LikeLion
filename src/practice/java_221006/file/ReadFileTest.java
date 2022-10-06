5568254a53583ce026f8d4807ad7435fd0282aaa
menuhwang <hmin0206@naver.com>
2022-10-06 15:12:05 +0900

    파일 목록 읽어오기

--- src/practice/java_221006/file/ReadFileTest.java
+++ src/practice/java_221006/file/ReadFileTest.java
@@ -1,17 +1,22 @@
 package practice.java_221006.file;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ReadFileTest {
     public static void main(String[] args) {
-        ReadFile readFile = new ReadFile("a_file.txt");
-        try {
-            System.out.println(readFile.readAChar()); // 한 글자 읽기
-            System.out.println(readFile.read2Char()); // 두 글자 읽기
-            System.out.println(readFile.readToString(5)); // n 글자 읽기
-            System.out.println(readFile.readALine());
-        } catch (IOException e) {
-            e.printStackTrace();
+//        ReadFile readFile = new ReadFile("a_file.txt");
+//        try {
+//            System.out.println(readFile.readAChar()); // 한 글자 읽기
+//            System.out.println(readFile.read2Char()); // 두 글자 읽기
+//            System.out.println(readFile.readToString(5)); // n 글자 읽기
+//            System.out.println(readFile.readALine());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        File dir = new File("./");
+        for (File file : dir.listFiles()) {
+            System.out.println(file);
         }
     }
 }
