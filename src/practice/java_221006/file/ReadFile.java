7b9410e22d5b5a0dc1b2e59dbd3c904a17b8054f
menuhwang <hmin0206@naver.com>
2022-10-06 15:35:23 +0900

    리팩토링
    - readAChar 메서드
    - read2Char 메서드

--- src/practice/java_221006/file/ReadFile.java
+++ src/practice/java_221006/file/ReadFile.java
@@ -7,17 +7,11 @@ import java.io.IOException;
 
 public class ReadFile {
     public char readAChar(String filename) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        return (char)br.read();
+        return readToString(filename, 1).charAt(0);
     }
 
     public String read2Char(String filename) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        String result = "";
-        for (int i = 0; i < 2; i++) {
-            result += (char)br.read();
-        }
-        return result;
+        return readToString(filename, 2);
     }
 
     public String readToString(String filename, int n) throws IOException {
