e29fcb326dee2db2bd0976b44dc3c32bd6589280
menuhwang <hmin0206@naver.com>
2022-10-06 09:23:44 +0900

    List 사용 복습

---
+++ src/practice/java_221006/collection/ListExerciseMain.java
@@ -0,0 +1,15 @@
+package practice.java_221006.collection;
+
+import java.util.List;
+
+public class ListExerciseMain {
+    public static void main(String[] args) {
+        ListExercise listExercise = new ListExercise();
+
+        List<String> students = listExercise.getStudents();
+
+        for (String student : students) {
+            System.out.println(student);
+        }
+    }
+}
