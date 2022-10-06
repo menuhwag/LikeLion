b577430604a49fb51ad830c95e097552096146d0
menuhwang <hmin0206@naver.com>
2022-10-06 10:09:43 +0900

    랜덤 숫자 생성기와 Set을 활용한 중복제거

--- src/practice/java_221006/collection/set_practice/RndNumberWithoutDuplicated.java
+++ src/practice/java_221006/collection/set_practice/RndNumberWithoutDuplicated.java
@@ -1,11 +1,16 @@
 package practice.java_221006.collection.set_practice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RndNumberWithoutDuplicated {
     public static void main(String[] args) {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        Set<Integer> setInteger = new HashSet<>();
         for (int i = 0; i < 50; i++) {
-            int num = randomNumberGenerator.generate(10);
-            System.out.println(num);
+            setInteger.add(randomNumberGenerator.generate(50));
         }
+        System.out.println("Set 개수 : " + setInteger.size());
+        System.out.println(setInteger);
     }
 }
