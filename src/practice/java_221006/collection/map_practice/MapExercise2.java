8a076fa20c58ea8b2951d0029415046e1f9a5e5d
menuhwang <hmin0206@naver.com>
2022-10-06 11:49:03 +0900

    알파벳 개수 세기 주석 추가

--- src/practice/java_221006/collection/map_practice/MapExercise2.java
+++ src/practice/java_221006/collection/map_practice/MapExercise2.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 
 public class MapExercise2 {
     public static void main(String[] args) {
-        String repo = "https://github.com/menuhwang/LikeLion".toLowerCase();
+        String repo = "https://github.com/menuhwang/LikeLion".toLowerCase(); // 대문자를 소문자로 변환
         HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < repo.length(); i++) {
-            if (repo.charAt(i) >= 'a' && repo.charAt(i) <= 'z') {
+        for (int i = 0; i < repo.length(); i++) { // repo 길이만큼 반복
+            if (repo.charAt(i) >= 'a' && repo.charAt(i) <= 'z') { // 문자인지 아닌지 구분
                 map.put(repo.charAt(i), map.getOrDefault(repo.charAt(i), 0) + 1);
+                // getOrDefault로 값을 가져온다, 없으면 0을 리턴해준다.
+                // 그리고 +1 한 값으로 설정해준다.
             }
         }
         System.out.println(map);
