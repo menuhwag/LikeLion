0d01b34f6a6d323c15a94ce8599260d2005fdc9b
menuhwang <hmin0206@naver.com>
2022-10-06 22:33:08 +0900

    20221006 과제
    - CodeUp 1021 ~ 1030번

---
+++ src/homework/homework_221006/codeup/_1023/Main.java
@@ -0,0 +1,12 @@
+package homework.homework_221006.codeup._1023;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] num = sc.nextLine().split("\\.");
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+    }
+}
---
+++ src/homework/homework_221006/codeup/_1028/Main.java
@@ -0,0 +1,11 @@
+package homework.homework_221006.codeup._1028;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        System.out.println(num);
+    }
+}
