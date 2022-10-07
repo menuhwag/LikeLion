7b4c586f0e82276ac231a6f0652a79242c94cd70
menuhwang <hmin0206@naver.com>
2022-10-07 23:48:41 +0900

    221007 과제
    - CodeUp 1031 ~ 1040

---
+++ src/homework/homework_221007/codeup/_1036/Main.java
@@ -0,0 +1,10 @@
+package homework.homework_221007.codeup._1036;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) sc.next().charAt(0));
+    }
+}
