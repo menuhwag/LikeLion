6c95c01bd6c67f912621d918d63394a2224ac568
menuhwang <hmin0206@naver.com>
2022-10-11 09:20:22 +0900

    프로그래머스 자릿수 합 구하기 새로운 방법

--- src/practice/java_221011/programmers/sum_digit/Solution.java
+++ src/practice/java_221011/programmers/sum_digit/Solution.java
@@ -3,11 +3,19 @@ package practice.java_221011.programmers.sum_digit;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str = String.valueOf(n);
-        for (int i = 0; i < str.length(); i++) {
-            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        while(n > 0) {
+            answer += n%10;
+            n = n / 10;
         }
-
         return answer;
     }
+//    public int solution(int n) {
+//        int answer = 0;
+//        String str = String.valueOf(n);
+//        for (int i = 0; i < str.length(); i++) {
+//            answer += Integer.parseInt(String.valueOf(str.charAt(i))); // 이게 최선인가...
+//        }
+//
+//        return answer;
+//    }
 }
