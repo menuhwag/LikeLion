a5920f9f1b8ad4a82db14d040c678e4ad970bf68
menuhwang <hmin0206@naver.com>
2022-10-11 09:13:36 +0900

    프로그래머스 자릿수 합 구하기

---
+++ src/practice/java_221011/programmers/sum_digit/Solution.java
@@ -0,0 +1,13 @@
+package practice.java_221011.programmers.sum_digit;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+        return answer;
+    }
+}
