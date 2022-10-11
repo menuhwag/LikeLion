f98717749cc5be0f9a115b3d8e210b58e064318c
menuhwang <hmin0206@naver.com>
2022-10-11 20:22:41 +0900

    221011 알고리즘 복습
    - 몫과 나머지 활용

---
+++ src/homework/homework_221011/programmers/sum_digit/Solution.java
@@ -0,0 +1,12 @@
+package homework.homework_221011.programmers.sum_digit;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+        return answer;
+    }
+}
