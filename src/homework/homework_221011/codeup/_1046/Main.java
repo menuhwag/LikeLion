8ec219cf9bc7beb4ffa2aff4a21c986929686fdf
menuhwang <hmin0206@naver.com>
2022-10-11 19:03:38 +0900

    221011 코드업 문제풀이 과제
    - CodeUp 1041 ~ 1050

---
+++ src/homework/homework_221011/codeup/_1046/Main.java
@@ -0,0 +1,16 @@
+package homework.homework_221011.codeup._1046;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a + b + c;
+        System.out.println((long)sum);
+        System.out.printf("%.1f", (float) sum / 3 );
+    }
+}
---
+++ src/homework/homework_221011/codeup/_1047/Main.java
@@ -0,0 +1,11 @@
+package homework.homework_221011.codeup._1047;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num << 1);
+    }
+}
