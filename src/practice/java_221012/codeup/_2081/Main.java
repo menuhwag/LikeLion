c26ba06ea5808665fe44e47f47eb482db3377e6d
menuhwang <hmin0206@naver.com>
2022-10-12 09:46:29 +0900

    20221012 CodeUp 2081 개선
    - 음수로 이루어진 배열입력 케이스

--- src/practice/java_221012/codeup/_2081/Main.java
+++ src/practice/java_221012/codeup/_2081/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int maxNum = -1;
+        int maxNum = -1; // 음수로만 이루어진 배열이 들어올 경우 정상 작동하지 않음.
         int maxIdx = -1;
 
         for (int i = 0; i < 9; i++) {
