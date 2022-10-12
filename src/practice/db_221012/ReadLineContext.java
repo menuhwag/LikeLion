d1166e7e9d7540eb297f9f3c3b834cf2c5857963
menuhwang <hmin0206@naver.com>
2022-10-12 17:09:59 +0900

    DB에 데이터 저장하기

---
+++ src/practice/db_221012/ReadLineContext.java
@@ -0,0 +1,44 @@
+package practice.db_221012;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadLineContext<T> {
+    private Parser<T> parser;
+
+    public ReadLineContext(Parser<T> parser) {
+        this.parser = parser;
+    }
+
+    public List<T> toList(String filename) {
+        List<T> list = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while((line = br.readLine()) != null) {
+                list.add(parser.parse(line));
+            }
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("파일 읽기 오류");
+        }
+        return list;
+    }
+
+    public void save(String filename, String body) {
+        File file = new File(filename);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(body);
+            writer.close();
+            System.out.println("파일 저장 완료!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
