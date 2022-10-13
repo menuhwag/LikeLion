dd1eaad7d52c651ffa2024cbe448a7971bb2765c
menuhwang <hmin0206@naver.com>
2022-10-13 09:48:23 +0900

    20221013 알고리즘 : 버블 정렬 수정

--- src/practice/java_221013/algorithm/bubblesort/BubbleSort1.java
+++ src/practice/java_221013/algorithm/bubblesort/BubbleSort1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort1 {
     public void sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) swap(arr, i, j);
             }
