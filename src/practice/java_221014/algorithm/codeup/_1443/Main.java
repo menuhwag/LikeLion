4227079a337c96434aae1de145bced74d763ab29
menuhwang <hmin0206@naver.com>
2022-10-14 10:27:08 +0900

    20221014 알고리즘 : 코드업 1443

---
+++ src/practice/java_221014/algorithm/codeup/_1443/Main.java
@@ -0,0 +1,39 @@
+package practice.java_221014.algorithm.codeup._1443;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] arr = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        new Main().sort(arr);
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+
+    public void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j-1] > arr[j]) {
+                    swap(arr, j-1, j);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
