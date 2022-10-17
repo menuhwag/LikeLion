8c8e784476cd1b3f4f6743bd4506b12bff4d80c3
menuhwang <hmin0206@naver.com>
2022-10-17 13:25:24 +0900

    221017 JDBC 연습
    - Hospital findById

--- src/week5/_221017/study/jdbc-practice/src/main/java/com/jdbcpractice/Application.java
+++ src/week5/_221017/study/jdbc-practice/src/main/java/com/jdbcpractice/Application.java
@@ -7,5 +7,10 @@ public class Application {
         HospitalDao hospitalDao = new HospitalDao();
 
         hospitalDao.add("D1000000", "인천광역시 남동구 구월동", "사랑내과");
+        Map<String, Object> hospital = hospitalDao.findById("D1000000");
+
+        for (String key : hospital.keySet()) {
+            System.out.printf("%s=%s\n", key, hospital.get(key));
+        }
     }
 }
--- src/week5/_221017/study/jdbc-practice/src/main/java/com/jdbcpractice/HospitalDao.java
+++ src/week5/_221017/study/jdbc-practice/src/main/java/com/jdbcpractice/HospitalDao.java
@@ -1,6 +1,7 @@
 package com.jdbcpractice;
 
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HospitalDao {
@@ -28,5 +29,37 @@ public class HospitalDao {
 
         ps.close();
         conn.close();
+    }
+
+    public Map<String, Object> findById(String id) throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String db_host = env.get("DB_HOST");
+        String db_user = env.get("DB_USER");
+        String db_pw = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conn = DriverManager.getConnection(
+                db_host, db_user, db_pw
+        );
+
+        PreparedStatement ps = conn.prepareStatement(
+                String.format("SELECT seoul.id, seoul.address, seoul.name FROM seoul WHERE seoul.id = '%s'", id)
+        );
+
+        ResultSet result = ps.executeQuery();
+
+        Map<String, Object> hospital = new HashMap<>();
+
+        if (result.next()){
+            hospital.put("id", result.getString(1));
+            hospital.put("address", result.getString(2));
+            hospital.put("name", result.getString(3));
+        }
+
+        ps.close();
+        conn.close();
+
+        return hospital;
     }
 }
