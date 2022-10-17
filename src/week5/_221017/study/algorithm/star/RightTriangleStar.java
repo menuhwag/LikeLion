34acd36f85213d414ec5b15998c506fe2db0620a
menuhwang <hmin0206@naver.com>
2022-10-17 09:08:13 +0900

    221017 알고리즘 : 별

---
+++ src/week5/_221017/study/algorithm/star/RightTriangleStar.java
@@ -0,0 +1,10 @@
+package week5._221017.study.algorithm.star;
+
+public class RightTriangleStar {
+    public void print() {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
---
+++ src/week5/_221017/study/algorithm/star/StarTest.java
@@ -0,0 +1,8 @@
+package week5._221017.study.algorithm.star;
+
+public class StarTest {
+    public static void main(String[] args) {
+        RightTriangleStar rightTriangleStar = new RightTriangleStar();
+        rightTriangleStar.print();
+    }
+}
