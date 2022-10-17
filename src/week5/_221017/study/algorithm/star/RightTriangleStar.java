ae21c9239cb073265219e71e470a86b82652e61d
menuhwang <hmin0206@naver.com>
2022-10-17 09:31:41 +0900

    221017 알고리즘 : 별

--- src/week5/_221017/study/algorithm/star/RightTriangleStar.java
+++ src/week5/_221017/study/algorithm/star/RightTriangleStar.java
@@ -1,10 +1,16 @@
 package week5._221017.study.algorithm.star;
 
 public class RightTriangleStar {
+    private char ch;
+
+    public RightTriangleStar(char ch) {
+        this.ch = ch;
+    }
+
     public void print(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i + 1; j++) {
-                System.out.print("*");
+                System.out.print(ch);
             }
             System.out.println();
         }
--- src/week5/_221017/study/algorithm/star/StarTest.java
+++ src/week5/_221017/study/algorithm/star/StarTest.java
@@ -2,7 +2,7 @@ package week5._221017.study.algorithm.star;
 
 public class StarTest {
     public static void main(String[] args) {
-        RightTriangleStar rightTriangleStar = new RightTriangleStar();
+        RightTriangleStar rightTriangleStar = new RightTriangleStar('#');
         rightTriangleStar.print(4);
     }
 }
