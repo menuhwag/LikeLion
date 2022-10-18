fa84a9f3a9efdaca9bb9487cc9cb6a930dd54458
menuhwang <hmin0206@naver.com>
2022-10-18 09:09:16 +0900

    20221018 알고리즘 : 별 정사각형

---
+++ src/week5/_221018/study/algorithm/star/Square.java
@@ -0,0 +1,12 @@
+package week5._221018.study.algorithm.star;
+
+public class Square {
+    public void print(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/week5/_221018/study/algorithm/star/StartTest.java
@@ -0,0 +1,8 @@
+package week5._221018.study.algorithm.star;
+
+public class StartTest {
+    public static void main(String[] args) {
+        Square square = new Square();
+        square.print(4);
+    }
+}
