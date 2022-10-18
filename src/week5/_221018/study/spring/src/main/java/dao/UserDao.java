d608e28d48048abeedb8601d134395f6ce0b3363
menuhwang <hmin0206@naver.com>
2022-10-18 11:45:02 +0900

    JDBC add, get 작성

---
+++ src/week5/_221018/study/spring/src/main/java/dao/UserDao.java
@@ -0,0 +1,81 @@
+package dao;
+
+import domain.User;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Map<String, String> env = System.getenv();
+
+        String DB_HOST = env.get("DB_HOST");
+        String DB_USER = env.get("DB_USER");
+        String DB_PASSWORD = env.get("DB_PASSWORD");
+
+
+        Connection c = DriverManager.getConnection(DB_HOST, DB_USER, DB_PASSWORD);
+
+        PreparedStatement ps = c.prepareStatement(
+                "INSERT INTO users(id, name, password) values (?, ?, ?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Map<String, String> env = System.getenv();
+
+        String DB_HOST = env.get("DB_HOST");
+        String DB_USER = env.get("DB_USER");
+        String DB_PASSWORD = env.get("DB_PASSWORD");
+
+        Connection c = DriverManager.getConnection(DB_HOST, DB_USER, DB_PASSWORD);
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?"
+        );
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+    }
+}
