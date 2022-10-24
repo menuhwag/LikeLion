c2090f0d397d7ba376b2395ca53215be5ff7cc2c
menuhwang <hmin0206@naver.com>
2022-10-24 10:14:50 +0900

    프로그래머스 : K번째수 PriorityQueue 활용

--- src/week6/_221024/study/algorithm/programmers/Kth/Solution.java
+++ src/week6/_221024/study/algorithm/programmers/Kth/Solution.java
@@ -1,6 +1,7 @@
 package week6._221024.study.algorithm.programmers.Kth;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int[] solution(int[] array, int[][] commands) {
@@ -13,11 +14,28 @@ public class Solution {
         }
         return answer;
     }
-
+    
+    public int[] solution2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int index = 0;
+        for (int[] command : commands) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for (int j = command[0] - 1; j < command[1]; j++) {
+                pq.add(array[j]);
+            }
+            int result = 0;
+            for (int i = 0; i < command[2]; i++) {
+                result = pq.poll();
+            }
+            answer[index++] = result;
+        }
+        return answer;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-        int[] answer = new Solution().solution(arr, commands);
+//        int[] answer = new Solution().solution(arr, commands);
+        int[] answer = new Solution().solution2(arr, commands);
         System.out.println(Arrays.toString(answer));
     }
 }
