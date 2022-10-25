864fa8275cb158d93350c3ff41050d5487efccb2
menuhwang <hmin0206@naver.com>
2022-10-25 10:24:35 +0900

    알고리즘 : 프로그래머스 완주하지 못한 선수
    - Hash 사용

---
+++ src/week6/_221025/study/algorithm/programmers/marathon/Solution.java
@@ -0,0 +1,25 @@
+package week6._221025.study.algorithm.programmers.marathon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String person : participant) {
+            hashMap.put(person, hashMap.getOrDefault(person, 0) + 1);
+        }
+        for (String person : completion) {
+            if (hashMap.get(person) == 1) hashMap.remove(person);
+            else hashMap.put(person, hashMap.get(person) - 1);
+        }
+
+        return hashMap.keySet().iterator().next();
+    }
+
+    public static void main(String[] args) {
+        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(new Solution().solution(participant,completion)); // vinko
+    }
+}
