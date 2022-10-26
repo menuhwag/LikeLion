12f53e54725b20e1f59bed81151aeacc4cec0e25
menuHwang <hmin0206@naver.com>
2022-10-26 15:37:07 +0900

    PathVariable

--- src/week6/_221026/study/hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/week6/_221026/study/hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping("")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/{value}")
+    public String pathValue(@PathVariable("value") String value) {
+        return "echo " + value;
     }
 }
