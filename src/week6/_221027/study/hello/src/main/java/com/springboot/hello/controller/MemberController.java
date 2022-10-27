a4f2b5f56a30944c1bbf465b19131f875e6e9f9d
menuHwang <hmin0206@naver.com>
2022-10-27 14:32:56 +0900

    ResponseEntity 실습

--- src/week6/_221027/study/hello/src/main/java/com/springboot/hello/controller/MemberController.java
+++ src/week6/_221027/study/hello/src/main/java/com/springboot/hello/controller/MemberController.java
@@ -1,13 +1,14 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
 public class MemberController {
     @PostMapping("")
-    public String member(@RequestBody MemberDto dto) {
-        return dto.toString();
+    public ResponseEntity<MemberDto> member(@RequestBody MemberDto dto) {
+        return ResponseEntity.ok(dto);
     }
 }
