e3bd0c6a130bb46ce3f9011e11c1b59eed6280e7
menuHwang <hmin0206@naver.com>
2022-11-01 14:00:52 +0900

    병원 정보 영속화

--- src/week7/_221101/study/hospital/src/main/java/com/likelion/hospital/HospitalApplication.java
+++ src/week7/_221101/study/hospital/src/main/java/com/likelion/hospital/HospitalApplication.java
@@ -1,13 +1,26 @@
 package com.likelion.hospital;
 
+import com.likelion.hospital.context.ReadLineContext;
+import com.likelion.hospital.dao.HospitalDao;
+import com.likelion.hospital.domain.Hospital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class HospitalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HospitalApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext context = SpringApplication.run(HospitalApplication.class, args);
+		ReadLineContext<Hospital> hospitalReadLineContext = (ReadLineContext<Hospital>) context.getBean("hospitalReadLineContext");
+		HospitalDao hospitalDao = context.getBean(HospitalDao.class);
+		List<Hospital> hospitals = hospitalReadLineContext.readByLine("fulldata.txt");
+		for (Hospital hospital : hospitals) {
+			hospitalDao.insert(hospital);
+		}
 	}
 
 }
