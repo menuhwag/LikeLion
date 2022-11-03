de694237dad7fd9a1ac6f082208b72a5e1d73b8b
menuHwang <hmin0206@naver.com>
2022-11-03 09:24:49 +0900

    Eratosthenes

---
+++ src/week7/_221103/study/algorithm/Eratosthenes.java
@@ -0,0 +1,15 @@
+package week7._221103.study.algorithm;
+
+public class Eratosthenes {
+    public static int countPrimeNumber(int num) {
+        int[] check = new int[num];
+        // number = index + 1;
+        check[0] = -1; // 1 제거
+        for (int i = 2; i * i <= num; i++) {
+            for (int j = 2; i * j <= num; j++) {
+                check[i * j - 1] = -1;
+            }
+        }
+
+    }
+}
