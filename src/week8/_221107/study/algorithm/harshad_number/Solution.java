eb511fce36f851d47345d3de78ff87c3e7b6e0de
menuHwang <hmin0206@naver.com>
2022-11-07 09:13:34 +0900

    나누어 떨어지는지 확인

--- src/week8/_221107/study/algorithm/harshad_number/Solution.java
+++ src/week8/_221107/study/algorithm/harshad_number/Solution.java
@@ -3,15 +3,17 @@ package week8._221107.study.algorithm.harshad_number;
 public class Solution {
     public boolean solution(int x) {
         int sum = 0;
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
+        int num = x;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
-        System.out.println(sum);
-        return true;
+        return x % sum == 0;
     }
 
     public static void main(String[] args) {
-        new Solution().solution(135);
+        Solution solution = new Solution();
+        System.out.println(solution.solution(12));
+        System.out.println(solution.solution(11));
     }
 }
