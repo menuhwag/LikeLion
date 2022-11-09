fb96ad37ad91f57a7c20609639d663093a30d64d
menuHwang <hmin0206@naver.com>
2022-11-09 10:44:16 +0900

    선택 정렬 : BiFunction 적용

--- src/week8/_221109/study/algorithm/select_sort/SelectSort.java
+++ src/week8/_221109/study/algorithm/select_sort/SelectSort.java
@@ -1,21 +1,23 @@
 package week8._221109.study.algorithm.select_sort;
 
+import java.util.function.BiFunction;
+
 public class SelectSort {
-    public int[] sort(int[] arr, Comparator comparator) {
+    public int[] sort(int[] arr, BiFunction<Integer, Integer, Boolean> comparator) {
         return sort(arr, 0, comparator);
     }
 
-    private int[] sort(int[] arr, int target , Comparator comparator) {
+    private int[] sort(int[] arr, int target , BiFunction<Integer, Integer, Boolean> comparator) {
         if (arr.length == target) return arr;
         int findIdx = searchVal(arr, target, comparator);
         swap(arr, target, findIdx);
         return sort(arr, target + 1, comparator);
     }
 
-    private int searchVal(int[] arr, int target, Comparator comparator) {
+    private int searchVal(int[] arr, int target, BiFunction<Integer, Integer, Boolean> comparator) {
         int findIdx = target;
         for (int i = target + 1; i < arr.length; i++) {
-            if (comparator.compare(arr[findIdx], arr[i])) {
+            if (comparator.apply(arr[findIdx], arr[i])) {
                 findIdx = i;
             }
         }
