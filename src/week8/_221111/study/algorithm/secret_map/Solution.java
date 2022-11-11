83852996a437554006f307b3c50d66a8cdac168a
menuHwang <hmin0206@naver.com>
2022-11-11 09:35:21 +0900

    비밀 지도 : 풀이 완료

--- src/week8/_221111/study/algorithm/secret_map/Solution.java
+++ src/week8/_221111/study/algorithm/secret_map/Solution.java
@@ -12,9 +12,9 @@ public class Solution {
             int decryption = arr1[i] | arr2[i];
             // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
             // 가장 앞자리가 0일때 문제 발생..!
-            String binary = Integer.toBinaryString(decryption).replaceAll("1", "#").replaceAll("0", " ");
-            binary = String.format("%" + n + "s", binary);
-            System.out.println(binary);
+            String binary = Integer.toBinaryString(decryption);
+            binary = String.format("%" + n + "s", binary).replaceAll("1", "#").replaceAll("0", " ");
+            answer.add(binary);
         }
         return answer.toArray(String[]::new);
     }
@@ -35,11 +35,11 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int n = 5;
-//        int[] arr1 = {9, 20, 28, 18, 11};
-//        int[] arr2 = {30, 1, 21, 17, 28};
-//        solution.solution(n, arr1, arr2);
-        System.out.println(solution.toBinary(3));
-        System.out.println(solution.toBinary(8));
+        int n = 6;
+        int[] arr1 = {46, 33, 33 ,22, 31, 50};
+        int[] arr2 = {27 ,56, 19, 14, 14, 10};
+        solution.solution(n, arr1, arr2);
+//        System.out.println(solution.toBinary(3));
+//        System.out.println(solution.toBinary(8));
     }
 }
