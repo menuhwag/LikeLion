f9cf0ce43b5618758cf1ed645de43edc6e2d11f6
menuHwang <hmin0206@naver.com>
2022-11-11 09:52:24 +0900

    비밀 지도 :직접 작성한 2진수 변환 메서드 활용

--- src/week8/_221111/study/algorithm/secret_map/Solution.java
+++ src/week8/_221111/study/algorithm/secret_map/Solution.java
@@ -12,34 +12,36 @@ public class Solution {
             int decryption = arr1[i] | arr2[i];
             // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
             // 가장 앞자리가 0일때 문제 발생..!
-            String binary = Integer.toBinaryString(decryption);
-            binary = String.format("%" + n + "s", binary).replaceAll("1", "#").replaceAll("0", " ");
+            String binary = toBinary(decryption, n);
+            binary = binary.replace("1", "#").replace("0", " ");
             answer.add(binary);
         }
         return answer.toArray(String[]::new);
     }
 
-    public String toBinary(int n) {
+    public String toBinary(int n, int displayLen) {
         Stack<Integer> stack = new Stack<>();
         int dec = n;
         while (dec > 0) {
             stack.push(dec % 2);
             dec /= 2;
         }
-        String answer = "";
-        while (!stack.isEmpty()) {
-            answer += stack.pop();
+        StringBuilder answer = new StringBuilder();
+        while (displayLen > 0) {
+            answer.append((displayLen > stack.size()) ? "0" : stack.pop());
+            displayLen--;
         }
-        return answer;
+        return answer.toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int n = 6;
-        int[] arr1 = {46, 33, 33 ,22, 31, 50};
-        int[] arr2 = {27 ,56, 19, 14, 14, 10};
-        solution.solution(n, arr1, arr2);
-//        System.out.println(solution.toBinary(3));
-//        System.out.println(solution.toBinary(8));
+//        int n = 6;
+//        int[] arr1 = {46, 33, 33 ,22, 31, 50};
+//        int[] arr2 = {27 ,56, 19, 14, 14, 10};
+//        solution.solution(n, arr1, arr2);
+        System.out.println(solution.toBinary(3, 4));
+        System.out.println(solution.toBinary(8, 4));
+        System.out.println(solution.toBinary(15, 8));
     }
 }
