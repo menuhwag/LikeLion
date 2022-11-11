ef4037c4cd99bc96a61a04730eddb951b15ec5fb
menuHwang <hmin0206@naver.com>
2022-11-11 09:29:39 +0900

    비밀 지도 : 이진수 로직

--- src/week8/_221111/study/algorithm/secret_map/Solution.java
+++ src/week8/_221111/study/algorithm/secret_map/Solution.java
@@ -2,6 +2,7 @@ package week8._221111.study.algorithm.secret_map;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
@@ -11,8 +12,34 @@ public class Solution {
             int decryption = arr1[i] | arr2[i];
             // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
             // 가장 앞자리가 0일때 문제 발생..!
-            answer.add(Integer.toBinaryString(decryption).replaceAll("1", "#").replaceAll("0", " "));
+            String binary = Integer.toBinaryString(decryption).replaceAll("1", "#").replaceAll("0", " ");
+            binary = String.format("%" + n + "s", binary);
+            System.out.println(binary);
         }
         return answer.toArray(String[]::new);
+    }
+
+    public String toBinary(int n) {
+        Stack<Integer> stack = new Stack<>();
+        int dec = n;
+        while (dec > 0) {
+            stack.push(dec % 2);
+            dec /= 2;
+        }
+        String answer = "";
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        int n = 5;
+//        int[] arr1 = {9, 20, 28, 18, 11};
+//        int[] arr2 = {30, 1, 21, 17, 28};
+//        solution.solution(n, arr1, arr2);
+        System.out.println(solution.toBinary(3));
+        System.out.println(solution.toBinary(8));
     }
 }
