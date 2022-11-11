6eed2f4a65d6159f047e88a73fe26e6c20596fa8
menuHwang <hmin0206@naver.com>
2022-11-11 09:09:41 +0900

    비밀 지도 : 풀이 계획

---
+++ src/week8/_221111/study/algorithm/secret_map/Solution.java
@@ -0,0 +1,13 @@
+package week8._221111.study.algorithm.secret_map;
+
+public class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        int[] map = new int[n];
+        for (int i = 0; i < arr1.length; i++) {
+            // 비트 or 연산으로 지도 그리기
+        }
+        // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
+        String[] answer = {};
+        return answer;
+    }
+}
