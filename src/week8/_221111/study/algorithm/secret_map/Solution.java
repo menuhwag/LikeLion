6f7e9e41939780adf69b217289bfe7c3aa9e2056
menuHwang <hmin0206@naver.com>
2022-11-11 09:16:54 +0900

    비밀 지도 : 문제1 첫 번째자리가 0으로 끝나는 경우

--- src/week8/_221111/study/algorithm/secret_map/Solution.java
+++ src/week8/_221111/study/algorithm/secret_map/Solution.java
@@ -1,13 +1,18 @@
 package week8._221111.study.algorithm.secret_map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        int[] map = new int[n];
-        for (int i = 0; i < arr1.length; i++) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             // 비트 or 연산으로 지도 그리기
+            int decryption = arr1[i] | arr2[i];
+            // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
+            // 가장 앞자리가 0일때 문제 발생..!
+            answer.add(Integer.toBinaryString(decryption).replaceAll("1", "#").replaceAll("0", " "));
         }
-        // 2진 스트링으로 바꾸고 1을 #으로 replace하여 answer에 추가
-        String[] answer = {};
-        return answer;
+        return answer.toArray(String[]::new);
     }
 }
