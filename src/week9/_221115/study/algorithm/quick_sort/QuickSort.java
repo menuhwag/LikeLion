0b1bc266ab796fd2e8cb9225e07b9c75da951dce
menuHwang <hmin0206@naver.com>
2022-11-15 10:29:06 +0900

    퀵 정렬 : 재귀 완료
    - 병합과정의 문제 발생
    - merge 메소드를 구현하여 해결

--- src/week9/_221115/study/algorithm/quick_sort/QuickSort.java
+++ src/week9/_221115/study/algorithm/quick_sort/QuickSort.java
@@ -5,20 +5,43 @@ import java.util.List;
 
 public class QuickSort {
     public int[] sort(int[] arr) {
-        int pivot = arr.length / 2;
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            list.add(num);
+        }
+        List<Integer> sortedList = sort(list);
+        int[] sorted = new int[sortedList.size()];
+        for (int i = 0; i < sortedList.size(); i++) {
+            sorted[i] = sortedList.get(i);
+        }
+        return sorted;
+    }
+
+    public List<Integer> sort(List<Integer> arr) {
+        if (arr.size() < 2) return arr;
+        int pivot = arr.size() / 2;
         List<Integer> mid = new ArrayList<>();
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
 
-        mid.add(arr[pivot]);
+        mid.add(arr.get(pivot)); // 기준
 
-        for (int num : arr) {
-            if (num < arr[pivot]) left.add(num);
-            else if (arr[pivot] <= num) right.add(num);
+        for (int i = 0; i < arr.size(); i++) {
+            if (i == pivot) continue;
+            int num = arr.get(i);
+            // 좌우 나누기
+            if (num < arr.get(pivot)) left.add(num);
+            else if (arr.get(pivot) <= num) right.add(num);
         }
-        System.out.printf("left : %s\n", left);
-        System.out.printf("mid : %s\n", mid);
-        System.out.printf("right : %s\n", right);
-        return null;
+//        System.out.printf("left : %s\n", left);
+//        System.out.printf("mid : %s\n", mid);
+//        System.out.printf("right : %s\n", right);
+        return merge(sort(left), mid, sort(right));
+    }
+
+    private List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
+        mid.addAll(1, right);
+        mid.addAll(0, left);
+        return mid;
     }
 }
--- src/week9/_221115/study/algorithm/quick_sort/QuickSortTest.java
+++ src/week9/_221115/study/algorithm/quick_sort/QuickSortTest.java
@@ -1,9 +1,12 @@
 package week9._221115.study.algorithm.quick_sort;
 
+import java.util.Arrays;
+
 public class QuickSortTest {
     public static void main(String[] args) {
         int[] arr = {20, 18, 5, 19, 5, 25, 40, 50};
         QuickSort quickSort = new QuickSort();
-        quickSort.sort(arr);
+        int[] sorted = quickSort.sort(arr);
+        System.out.println(Arrays.toString(sorted));
     }
 }
