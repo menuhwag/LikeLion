b3ffbd3786a3f1f140c1a91d7e16c685a7a5bcfa
menuHwang <hmin0206@naver.com>
2022-11-16 10:56:43 +0900

    퀵 정렬 : 완성?
    - 주먹구구식으로 완성

--- src/week9/_221116/study/algorithm/quick_sort/QuickSort.java
+++ src/week9/_221116/study/algorithm/quick_sort/QuickSort.java
@@ -2,12 +2,20 @@ package week9._221116.study.algorithm.quick_sort;
 
 public class QuickSort {
     public void sort(int[] arr, int startIdx, int endIdx) {
+        if (startIdx >= endIdx) return;
+        int p = part(arr, startIdx, endIdx);
+        sort(arr, startIdx, p);
+        sort(arr, p + 1, endIdx);
+    }
+
+    private int part(int[] arr, int startIdx, int endIdx) {
         int pivot = arr[(startIdx + endIdx + 1) / 2];
         while (startIdx < endIdx) {
             while (arr[startIdx] < pivot) startIdx++;
             while (pivot < arr[endIdx]) endIdx--;
             swap(arr, startIdx++, endIdx--);
         }
+        return Math.min(startIdx, endIdx);
     }
 
     private void swap(int[] arr, int idx1, int idx2) {
