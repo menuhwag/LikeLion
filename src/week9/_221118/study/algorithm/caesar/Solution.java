1ed7085106e7a62190a070f47be4ea102872d1e3
menuHwang <hmin0206@naver.com>
2022-11-18 09:39:35 +0900

    시저 암호 : 채점 실패

--- src/week9/_221118/study/algorithm/caesar/Solution.java
+++ src/week9/_221118/study/algorithm/caesar/Solution.java
@@ -4,7 +4,13 @@ class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            char ch = (char)(s.charAt(i) + n);
+            char ch = s.charAt(i);
+            if (ch != ' '){
+                ch += n;
+                if (('Z' < ch && 'a' > ch ) || ch > 'z') {
+                    ch -= 26;
+                }
+            }
             sb.append(ch);
         }
         return sb.toString();
