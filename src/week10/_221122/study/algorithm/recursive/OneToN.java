14d688ffd60f9b665d140b0b70b2525b9f458f3d
menuHwang <hmin0206@naver.com>
2022-11-22 09:18:43 +0900

    재귀 : 완료

--- src/week10/_221122/study/algorithm/recursive/OneToN.java
+++ src/week10/_221122/study/algorithm/recursive/OneToN.java
@@ -1,13 +1,13 @@
 package week10._221122.study.algorithm.recursive;
 
 public class OneToN {
-    public static void print1ToN(int start) {
-        if (start > 100) return;
+    public static void print1ToN(int start, int end) {
+        if (start > end) return;
         System.out.println(start);
-        print1ToN(start + 1);
+        print1ToN(start + 1, end);
     }
 
     public static void main(String[] args) {
-        OneToN.print1ToN(1);
+        OneToN.print1ToN(1, 100);
     }
 }
