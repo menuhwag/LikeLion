6acdc60cde12c028a73fd10162f2b9e299b15ec9
menuHwang <hmin0206@naver.com>
2022-11-22 09:50:06 +0900

    List에 있는 모든 값더하기 : 파라미터 한 개 만으로?

--- src/week10/_221122/study/algorithm/recursive/SummaryList.java
+++ src/week10/_221122/study/algorithm/recursive/SummaryList.java
@@ -7,8 +7,12 @@ import java.util.List;
 public class SummaryList {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(7, 3, 2, 9));
-        int sum = SummaryList.summary(list, 0);
+        int sum = SummaryList.summary(list);
         System.out.println(sum);
+    }
+
+    private static int summary(List<Integer> list) {
+        return summary(list, 0);
     }
 
     private static int summary(List<Integer> list, int init) {
