c1fd7c02862fc2a860cf67ec97b8a507bfa73eab
menuHwang <hmin0206@naver.com>
2022-11-22 09:17:46 +0900

    재귀 2단계 : 탈출 조건 추가

--- src/week10/_221122/study/algorithm/recursive/OneToN.java
+++ src/week10/_221122/study/algorithm/recursive/OneToN.java
@@ -2,8 +2,9 @@ package week10._221122.study.algorithm.recursive;
 
 public class OneToN {
     public static void print1ToN(int start) {
+        if (start > 100) return;
         System.out.println(start);
-        print1ToN(start);
+        print1ToN(start + 1);
     }
 
     public static void main(String[] args) {
