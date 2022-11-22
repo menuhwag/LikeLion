ce546c7546b6d87a23a3d207babcdb4f0614d903
menuHwang <hmin0206@naver.com>
2022-11-22 10:20:33 +0900

    array에 있는 모든 값더하기 : 수정

--- src/week10/_221122/study/algorithm/recursive/SummaryArray.java
+++ src/week10/_221122/study/algorithm/recursive/SummaryArray.java
@@ -3,12 +3,12 @@ package week10._221122.study.algorithm.recursive;
 public class SummaryArray {
     public static void main(String[] args) {
         int[] arr = {7, 3, 2, 9};
-        int sum = SummaryArray.summary(arr, 0, 0);
+        int sum = SummaryArray.summary(arr, 0);
         System.out.println(sum);
     }
 
-    private static int summary(int[] arr, int idx, int init) {
-        if (arr.length <= idx) return init;
-        return summary(arr, idx + 1, init + arr[idx]);
+    private static int summary(int[] arr, int idx) {
+        if (arr.length <= idx) return 0;
+        return arr[idx] + summary(arr, idx + 1);
     }
 }
