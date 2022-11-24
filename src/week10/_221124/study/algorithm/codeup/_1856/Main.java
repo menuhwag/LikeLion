50b4c8a3201a33b0b3515e7297ba713a03ed086a
menuHwang <hmin0206@naver.com>
2022-11-24 10:08:43 +0900

    [CodeUp] 1856 계단 뛰어 오르기 : 완료

--- src/week10/_221124/study/algorithm/codeup/_1856/Main.java
+++ src/week10/_221124/study/algorithm/codeup/_1856/Main.java
@@ -1,4 +1,18 @@
 package week10._221124.study.algorithm.codeup._1856;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Main.solution(scanner.nextInt()));
+    }
+
+    private static int solution(int n) {
+        if (n < 1) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 4;
+        return solution(n - 1) + solution(n - 2) + solution(n - 3);
+    }
 }
