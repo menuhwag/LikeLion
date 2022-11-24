a197a4913f76abd7fced29656e596d28469203c8
menuHwang <hmin0206@naver.com>
2022-11-24 12:54:23 +0900

    [CodeUp] 1862 재귀로 n번째 피보나치 수 출력하기(메모이제이션) : 완료

--- src/week10/_221124/study/algorithm/codeup/_1862/Main.java
+++ src/week10/_221124/study/algorithm/codeup/_1862/Main.java
@@ -6,11 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(fib(n) % 1_000_000_007);
+        System.out.println(fib(n));
     }
 
-    private static int fib(int n) {
-        if (n <= 2) return 1;
-        return fib(n - 1) + fib(n - 2);
+    private static boolean[] chk = new boolean[101];
+    private static long[] val = new long[101];
+    private static long fib(int n) {
+        if (chk[n]) return val[n];
+        long fib;
+        if (n <= 2) fib = 1;
+        else fib = fib(n - 1) + fib(n - 2);
+        chk[n] = true;
+        fib %= 1_000_000_007;
+        val[n] = fib;
+        return fib;
     }
 }
