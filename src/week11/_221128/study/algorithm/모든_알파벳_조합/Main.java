3cf85f5e71c5e46546990a990a3d97a50c649f4f
menuHwang <hmin0206@naver.com>
2022-11-28 09:51:11 +0900

    [Algo] 모든 알파벳 조합 : 재귀 적용

--- src/week11/_221128/study/algorithm/모든_알파벳_조합/Main.java
+++ src/week11/_221128/study/algorithm/모든_알파벳_조합/Main.java
@@ -2,21 +2,23 @@ package week11._221128.study.algorithm.모든_알파벳_조합;
 
 public class Main {
     public static void main(String[] args) {
-        print1();
-        print2();
+//        print1('A');
+        print2('A', 'A');
     }
 
-    private static void print1() {
-        for (char c = 'A'; c <= 'Z'; c++) {
-            System.out.println(c);
-        }
+    private static void print1(char c) {
+        if (c > 'Z') return;
+        System.out.println(c);
+        print1((char) (c + 1));
     }
 
-    private static void print2() {
-        for (char c = 'A'; c <= 'Z'; c++) {
-            for (char d = 'A'; d <= 'Z'; d++) {
-                System.out.printf("%s%s\n", c, d);
-            }
+    private static void print2(char c, char d) {
+        if (c > 'Z') return;
+        if (d > 'Z') {
+            print2((char) (c + 1), 'A');
+            return;
         }
+        System.out.printf("%s%s\n", c, d);
+        print2(c, (char) (d + 1));
     }
 }
