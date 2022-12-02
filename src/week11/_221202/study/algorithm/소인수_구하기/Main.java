0befda95328595fd6f43120f3380be1210e897e0
menuHwang <hmin0206@naver.com>
2022-12-02 10:41:08 +0900

    [Algo] 소인수 구하기 : 주석 추가
    - div 타입 수정 후 채점

--- src/week11/_221202/study/algorithm/소인수_구하기/Main.java
+++ src/week11/_221202/study/algorithm/소인수_구하기/Main.java
@@ -11,6 +11,12 @@ public class Main {
         long answer=1L;
 
         for (long div = 2L; div * div < num; div++) { // 반복문 범위를 제곱근으로 줄였습니다.
+            /* 실험.
+            * 루트 80억은 약 8만 9천 4백
+            * div를 int로 바꿀 수 있지 않을까해서 int로 바꾸고 채점해보았다.
+            * 그러나 시간 초과 발생.
+            * 아무래도 캐스팅하는데 시간이 오래 걸리는것 같다.
+            * */
             while (temp % div == 0L) { // 나머지 없이 딱 나누어 떨어진다면
                 temp /= div; // 몫으로 덮어쓰고
                 answer = div; // 나눈 값을 저장합니다.
