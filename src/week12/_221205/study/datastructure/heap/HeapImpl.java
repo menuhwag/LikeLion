4356c199e8f232afd90854684efc6822800d7d0c
menuHwang <hmin0206@naver.com>
2022-12-05 09:33:27 +0900

    [DS] 힙 구현

---
+++ src/week12/_221205/study/datastructure/heap/HeapImpl.java
@@ -0,0 +1,39 @@
+package week12._221205.study.datastructure.heap;
+
+import java.util.Arrays;
+
+public class HeapImpl {
+    int[] heap;
+    public HeapImpl(int[] arr) {
+        this.heap = arr;
+        sort(arr.length);
+    }
+
+    private void sort(int index) {
+        if (index <= 1) return;
+        int chileIdx = index - 1;
+        int parentIdx = getParent(index) - 1;
+        if (heap[chileIdx] > heap[parentIdx]) {
+            swap(chileIdx, parentIdx);
+            sort(parentIdx + 1);
+        } else {
+            return;
+        }
+        sort(index - 1);
+    }
+
+    private int getParent(int childIdx) {
+        return childIdx / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(heap);
+    }
+}
---
+++ src/week12/_221205/study/datastructure/heap/HeapImplTest.java
@@ -0,0 +1,8 @@
+package week12._221205.study.datastructure.heap;
+
+public class HeapImplTest {
+    public static void main(String[] args) {
+        HeapImpl heap = new HeapImpl(new int[]{6, 5, 7, 8});
+        System.out.println(heap);
+    }
+}
