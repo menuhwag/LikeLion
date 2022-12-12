64e4ea59035f96a847341a65b762e896f4f58f26
menuHwang <hmin0206@naver.com>
2022-12-12 09:57:06 +0900

    [Algo] DP MinCost 리팩토링

--- src/week13/_221212/study/algorithm/min_cost/MinCost.java
+++ src/week13/_221212/study/algorithm/min_cost/MinCost.java
@@ -8,19 +8,17 @@ public class MinCost {
 
     public MinCost(int[][] matrix) {
         this.matrix = matrix;
-        this.memo = new int[matrix.length + 1][matrix[0].length + 1];
-        this.memo[1][1] = this.matrix[0][0];
+        this.memo = new int[matrix.length][matrix[0].length];
+        this.memo[0][0] = this.matrix[0][0];
         findPath();
     }
 
     private void findPath() {
-        for (int i = 1; i < memo.length; i++) {
-            for (int j = 1; j < memo[i].length; j++) {
-                int minCost = 0;
-                minCost = (minCost == 0) ? this.memo[i][j - 1] : (this.memo[i][j - 1] == 0) ? minCost : Math.min(minCost, this.memo[i][j - 1]);
-                minCost = (minCost == 0) ? this.memo[i - 1][j - 1] : (this.memo[i - 1][j - 1] == 0) ? minCost : Math.min(minCost, this.memo[i - 1][j - 1]);
-                minCost = (minCost == 0) ? this.memo[i - 1][j] : (this.memo[i - 1][j] == 0) ? minCost : Math.min(minCost, this.memo[i - 1][j]);
-                this.memo[i][j] = minCost + this.matrix[i - 1][j - 1];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[i].length; j++) {
+                if (i == 0 && j != 0) this.memo[i][j] = this.memo[i][j - 1] + this.matrix[i][j];
+                else if (i != 0 && j == 0) this.memo[i][j] = this.memo[i - 1][j] + this.matrix[i][j];
+                else if (i != 0 && j != 0) this.memo[i][j] = Math.min(this.memo[i][j - 1], Math.min(this.memo[i - 1][j - 1], this.memo[i - 1][j])) + this.matrix[i][j];
             }
         }
     }
