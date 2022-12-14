2222a05414755aac67efc032481eb491b468e619
menuHwang <hmin0206@naver.com>
2022-12-14 10:43:03 +0900

    [Algo] 최적 전략 : 카드 게임

--- src/week13/_221213/study/algorithm/CardGame.java
+++ src/week13/_221213/study/algorithm/CardGame.java
@@ -1,7 +1,5 @@
 package week13._221213.study.algorithm;
 
-import java.util.Arrays;
-
 public class CardGame {
     int[] cards;
     Pair[][] memo;
@@ -9,22 +7,32 @@ public class CardGame {
     public CardGame(int[] cards) {
         this.cards = cards;
         this.memo = new Pair[cards.length][cards.length];
-        strategy();
-    }
-
-    private void strategy() {
         for (int i = 0; i < memo.length; i++) {
             memo[i][i] = new Pair(cards[i], 0);
         }
-        for (int i = 0; i < memo.length - 1; i++) {
-            memo[i][i + 1] = new Pair(Math.max(cards[i], cards[i + 1]), Math.min(cards[i], cards[i + 1]));
+        for (int i = 1; i < memo.length; i++) {
+            for (int j = 0; j + i < memo.length; j++) {
+                memo[j][j + i] = strategy(cards[j], cards[j + i], memo[j][j + i - 1], memo[j + 1][j + i]);
+            }
         }
+    }
+
+    private Pair strategy(int leftCard, int rightCard, Pair leftPair, Pair bottomPair) {
+        // 내가 오른쪽 카드를 뽑으면
+        int left = rightCard + leftPair.right;
+        int right = leftPair.left;
+        Pair pair1 = new Pair(left, right);
+        // 내가 왼쪽 카드를 뽑으면
+        left = leftCard + bottomPair.right;
+        right = bottomPair.left;
+        Pair pair2 = new Pair(left, right);
+        return Pair.best(pair1, pair2);
     }
 
     public void print() {
         for (Pair[] row : memo) {
             for (Pair element : row) {
-                System.out.printf("%s\t", element);
+                System.out.printf("%-10s", element);
             }
             System.out.println();
         }
@@ -38,6 +46,20 @@ class Pair {
     public Pair(int left, int right) {
         this.left = left;
         this.right = right;
+    }
+
+    public static Pair best(Pair a, Pair b) {
+        if (a.equals(b)) return a;
+        return (a.left > b.left) ? a : b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Pair) {
+            Pair pair = (Pair) obj;
+            if (this.left == pair.left && this.right == pair.right) return true;
+        }
+        return false;
     }
 
     @Override
--- src/week13/_221213/study/algorithm/CardGameTest.java
+++ src/week13/_221213/study/algorithm/CardGameTest.java
@@ -3,7 +3,12 @@ package week13._221213.study.algorithm;
 public class CardGameTest {
     public static void main(String[] args) {
         int[] cards = {2, 7, 40, 19};
-        CardGame cardGame = new CardGame(cards);
-        cardGame.print();
+        CardGame cardGame1 = new CardGame(cards);
+        System.out.println("게임 1 :");
+        cardGame1.print();
+        cards = new int[]{2, 7, 40, 19, 4, 9};
+        CardGame cardGame2 = new CardGame(cards);
+        System.out.println("게임 2 :");
+        cardGame2.print();
     }
 }
